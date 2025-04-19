cbee3e44cd834286a0e5a34d150dc360b4490b0f
Abhiraj Chaudhuri <abhirajchaudhuri@gmail.com>
2025-04-19 17:34:42 +0530

    first commit; base file structure

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Page from "@/app/dashboard/page"
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Page/>
+    </div>
+  )
+}
+
+export default App
